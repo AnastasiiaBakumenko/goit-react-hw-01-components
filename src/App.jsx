e946f838d8093307063a8a41a782942b9f645f9e
AnastasiiaBakumenko <bakumenkoanastasia2@gmail.com>
2023-03-12 14:35:15 +0100

    add task-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Section from "./components/section/Section";
 import SocialProfile from "./components/SocialProfile/SocialProfile";
 import Statistics from "./components/Statistics/Statistics";
-// import FriendList from "./components/FriendList/FriendList";
-// import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 import user from "./data/user.json";
 import data from "./data/data.json";
-// import friends from "./data/friends.json";
-// import transactions from "./data/transactions.json";
+import friends from "./data/friends.json";
+import transactions from "./data/transactions.json";
 
 export default function App() {
   return (
@@ -28,14 +28,14 @@ export default function App() {
       <Section>
         <Statistics stats={data} />
       </Section>
-{/* 
+
       <Section title={"Task-3 'Friend list'"}>
         <FriendList friends={friends} />
       </Section>
 
       <Section title={"Task-4 'Transaction history'"}>
         <TransactionHistory transactions={transactions} />
-      </Section>  */}
+      </Section> 
     </div>
   );
 }
